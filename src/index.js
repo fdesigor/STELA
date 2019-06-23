5d442c6d4e87597d6e3473e5e859963ca49dc44c
Igor Fernandes <fdesigor@gmail.com>
2019-06-23 03:39:56 -0300

    Funcionalidades de persistencia no mongodb

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const path = require('path')
+require('dotenv/config')
 
 const app = express()
+
+mongoose.connect(`${process.env.DATABASE_URL}`, {
+    useNewUrlParser: true
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
 
 app.use(require('./routes'))
 
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,34 @@
 const routes = require('express').Router()
+const multer = require('multer')
+const multerConfig = require('./config/multer')
+require('dotenv/config')
 
-routes.get('/', (req, res) => {
-    return res.json({ title: 'Hello STELA'})
+const Post = require('./models/Post')
+
+routes.post('/create', multer(multerConfig).single('file'), async (req, res) => {
+    const { originalname: name, size, filename: key } = req.file
+
+    const post = await Post.create({
+        name,
+        size,
+        key,
+        url: `${process.env.APP_URL}files/${key}`
+    })
+
+    return res.json(post)
+})
+
+routes.get('/read', async (req, res) => {
+    const post = await Post.find()
+
+    return res.json(post)
+})
+
+routes.delete('/delete/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    await post.remove()
+    
+    return res.send()
 })
 
 module.exports = routes
